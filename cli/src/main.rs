8bd66fe26a707e393c971947d309d282956a779d
Clifton King <cliftonk@gmail.com>
2022-10-17 18:23:32 +0200

    add erc-20 balance

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,21 @@
+use std::sync::Arc;
+
+use bindings::ierc20_metadata::IERC20Metadata;
 use clap::{Parser, Subcommand};
 use ethers::prelude::*;
 use eyre::Result;
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    #[clap(about = "Get the balance of an account")]
+    #[clap(about = "Get the native token balance of an account")]
     Balance {
+        #[clap(help = "The address to get the balance of")]
+        address: Address,
+    },
+    #[clap(about = "Get the erc20 token balance of an account")]
+    Erc20Balance {
+        #[clap(help = "The token address")]
+        token: Address,
         #[clap(help = "The address to get the balance of")]
         address: Address,
     },
@@ -27,12 +37,21 @@ struct CliOpts {
 async fn main() -> Result<()> {
     let CliOpts { rpc_url, cmd, .. } = CliOpts::parse();
 
+    let provider = Arc::new(Provider::<Http>::try_from(rpc_url)?);
+
     match cmd {
         Command::Balance { address } => {
-            let provider = Provider::<Http>::try_from(rpc_url)?;
             let balance = provider.get_balance(address, None).await?;
             let balance_str = ethers::utils::format_units(balance, "ether")?;
             println!("Balance: {}", balance_str);
+        }
+        Command::Erc20Balance { token, address } => {
+            let contract = IERC20Metadata::new(token, provider.clone());
+            let decimals = contract.decimals().call().await?;
+            let balance = contract.balance_of(address).call().await?;
+            let symbol = contract.symbol().call().await?;
+            let balance_f64 = balance.as_u128() as f64 / 10_f64.powi(decimals.into());
+            println!("{} {}", balance_f64, symbol);
         }
     }
     Ok(())
