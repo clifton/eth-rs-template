8373d9d3164ac145283bec24b889d6a6cca0ce4f
Clifton King <cliftonk@gmail.com>
2022-10-17 17:53:20 +0200

    add --rpc-url

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,20 +13,23 @@ enum Command {
 
 #[derive(Parser, Debug)]
 struct CliOpts {
-    // rpc_url: String,
+    #[clap(
+        long,
+        env = "RPC_URL",
+        default_value = "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27"
+    )]
+    rpc_url: String,
     #[clap(subcommand)]
     cmd: Command,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli = CliOpts::parse();
+    let CliOpts { rpc_url, cmd, .. } = CliOpts::parse();
 
-    match cli.cmd {
+    match cmd {
         Command::Balance { address } => {
-            let provider = Provider::<Http>::try_from(
-                "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27",
-            )?;
+            let provider = Provider::<Http>::try_from(rpc_url)?;
             let balance = provider.get_balance(address, None).await?;
             let balance_str = ethers::utils::format_units(balance, "ether")?;
             println!("Balance: {}", balance_str);
